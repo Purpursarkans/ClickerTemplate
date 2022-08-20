3bd1ed9fcde154bdba54ca9dd028577b86152111
Purpursarkans <goga.danilov10@gmail.com>
2022-08-20 21:36:46 +0400

    update

--- ClickerTemplate.cpp
+++ ClickerTemplate.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include "Head.hpp"
+
+void LClickOnCoord(POINT &P, int X, int Y)
+{
+    GetCursorPos(&P);
+    SetCursorPos(X, Y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+}
+
+void RClickOnCoord(POINT &P, int X, int Y)
+{
+    GetCursorPos(&P);
+    SetCursorPos(X, Y);
+    mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, 0, 0);
+    mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
+}
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    HWND hwnd = FindWindowA("NULL", "DeadByDaylight");
+    std::cout << hwnd << std::endl;
+    if(hwnd == 0)
+        exit(1);
+    Sleep(5000);
+    
+    
+    
+    SendMessageA(hwnd, MOUSEEVENTF_RIGHTDOWN,0,0);
+    Sleep(5000);
+    SendMessageA(hwnd, MOUSEEVENTF_RIGHTUP, 0, 0);
+
+    POINT P;
+    RClickOnCoord(P, 10, 10);
+    //pause();
 }
---
+++ Head.hpp
@@ -0,0 +1,9 @@
+#ifndef HEAD_HPP
+#define HEAD_HPP
+
+#include <Windows.h>
+#include <iostream>
+#include "Cross/Cross.hpp"
+
+
+#endif
