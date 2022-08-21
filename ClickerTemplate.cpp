f961238c6a447987a75448ddedafe997d95d91fa
Purpursarkans <goga.danilov10@gmail.com>
2022-08-21 21:25:39 +0400

    update

--- ClickerTemplate.cpp
+++ ClickerTemplate.cpp
@@ -1,36 +1,57 @@
 #include "Head.hpp"
 
-void LClickOnCoord(POINT &P, int X, int Y)
+void LClickOnCoord(int X, int Y)
 {
-    GetCursorPos(&P);
     SetCursorPos(X, Y);
     mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
     mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
 }
 
-void RClickOnCoord(POINT &P, int X, int Y)
+void LClickOnCoord(POINT P)
 {
-    GetCursorPos(&P);
+    SetCursorPos(P.x, P.y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, P.x, P.y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, P.x, P.y, 0, 0);
+}
+void RClickOnCoord(POINT P)
+{
+    SetCursorPos(P.x, P.y);
+    mouse_event(MOUSEEVENTF_LEFTDOWN, P.x, P.y, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, P.x, P.y, 0, 0);
+}
+
+void RClickOnCoord(int X, int Y)
+{
     SetCursorPos(X, Y);
     mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, 0, 0);
     mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
 }
 
+void Timeout(int sleep)
+{
+    Sleep(sleep);
+    Beep(100, 100);
+}
+
 int main()
 {
-    HWND hwnd = FindWindowA("NULL", "DeadByDaylight");
-    std::cout << hwnd << std::endl;
-    if(hwnd == 0)
-        exit(1);
-    Sleep(5000);
-    
-    
-    
-    SendMessageA(hwnd, MOUSEEVENTF_RIGHTDOWN,0,0);
-    Sleep(5000);
-    SendMessageA(hwnd, MOUSEEVENTF_RIGHTUP, 0, 0);
-
+    //Timeout(5000);
     POINT P;
-    RClickOnCoord(P, 10, 10);
-    //pause();
+    GetCursorPos(&P);
+    //HWND hwnd = FindWindowA("NULL", "DeadByDaylight");
+
+    HWND hwnd = WindowFromPoint(P);
+    
+    std::cout << hwnd << std::endl;
+    Timeout(5000);
+
+    LClickOnCoord(P.x, P.y);
+
+    SendMessageA(hwnd, MOUSEEVENTF_LEFTDOWN, 0, MAKELPARAM(P.x, P.y));
+    Timeout(500);
+    SendMessageA(hwnd, MOUSEEVENTF_LEFTUP, 0, MAKELPARAM(P.x, P.y));
+
+    //RClickOnCoord(10, 10);
+    Beep(1000, 1000);
+    pause();
 }
